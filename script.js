2e6aa43de7638013e06f7c1cf79a2e7960bed03b
yorhs_malasquez <yorhs1688@gmail.com>
2023-06-14 23:17:00 -0500

    Finalizando el Componente interactivo

---
+++ script.js
@@ -0,0 +1,20 @@
+const submit = document.getElementById('submit');
+const number = document.getElementById('var');
+const valor = document.querySelectorAll('.btn-num');
+const submit2 = document.getElementById('submit2');
+submit.addEventListener("click",()=>{
+    document.getElementById('thank-you').style.display = 'block';
+    document.getElementById('principal').style.display = 'none';
+})
+
+valor.forEach(num => {
+    num.addEventListener('click', (event) => {
+        number.innerHTML = num.innerHTML;
+        console.log("valor", number.innerHTML);
+    });
+});
+submit2.addEventListener("click",()=>{
+    document.getElementById('thank-you').style.display = 'none';
+    document.getElementById('principal').style.display = 'block';
+})
+
